c3e41614aa9207c1fd3e59d7129e2a61eed88736
Luan <132374218+Luanhts@users.noreply.github.com>
2025-03-22 09:43:45 -0300

    feat: card livros

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body class="bg-stone-950 text-stone-200">
     <header class="bg-stone-900">
-        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
             <div class="font-bold text-xl tracking-wide">Book Manager</div>
 
             <ul class="flex space-x-4 font-bold">
@@ -23,8 +23,84 @@
         </nav>
     </header>
 
-    <main>
-        Book List
+    <main class="mx-auto max-w-screen-lg space-y-6">
+
+        <form class="w-full flex space-x-2 mt-6">
+            <input type="text" class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
+                placeholder="Search...">
+
+            <button type="submit">Search</button>
+        </form>
+
+        <!--Lista de Livros-->
+        <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+            <!--Livro-->
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class=" flex">
+                    <div class="w-1/3">imagem</div>
+
+                    <div>
+                        <a href="/livro.php" class="font-semibold hover:underline">Titulo</a>
+                        <div class="text-xs italic">Altor</div>
+                        <div class="text-xs italic">(3) Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descrição
+                </div>
+            </div>
+
+            <!---->
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class=" flex">
+                    <div class="w-1/3">imagem</div>
+
+                    <div>
+                        <a href="/livro.php?" class="font-semibol hover:underline">Titulo</a>
+                        <div class="text-xs italic">Altor</div>
+                        <div class="text-xs italic">(3) Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descrição
+                </div>
+            </div>
+
+            <!---->
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class=" flex">
+                    <div class="w-1/3">imagem</div>
+
+                    <div>
+                        <a href="/livro.php?" class="font-semibol hover:underline">Titulo</a>
+                        <div class="text-xs italic">Altor</div>
+                        <div class="text-xs italic">(3) Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descrição
+                </div>
+            </div>
+
+            <!---->
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class=" flex">
+                    <div class="w-1/3">imagem</div>
+
+                    <div>
+                        <a href="/livro.php?" class="font-semibol hover:underline">Titulo</a>
+                        <div class="text-xs italic">Altor</div>
+                        <div class="text-xs italic">(3) Avaliações</div>
+                    </div>
+                </div>
+                <div class="text-sm">
+                    Descrição
+                </div>
+            </div>
+
+        </section>
+
     </main>
 </body>
 
