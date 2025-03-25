688ce38e84f9297bf221bbd5517d517f47eea561
Luan <132374218+Luanhts@users.noreply.github.com>
2025-03-25 18:52:25 -0300

    Criação dos controllers

---
+++ controllers/index.controller.php
@@ -0,0 +1,11 @@
+<?php
+
+//DADOS MODEL
+require 'dados.php';
+
+
+
+
+$view = "index";
+
+require "views/template/app.php";
--- index.php
+++ index.php
@@ -1,68 +1,9 @@
 <?php
 
-require_once("dados.php");
-?>
+$controller = "index";
 
-<!DOCTYPE html>
-<html lang="en">
+if (isset($_SERVER['PATH_INFO'])) {
+    $controller = str_replace('/', '', $_SERVER['PATH_INFO']);
+};
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Book Manager</title>
-</head>
-
-<body class="bg-stone-950 text-stone-200">
-    <header class="bg-stone-900">
-        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
-            <div class="font-bold text-xl tracking-wide">Book Manager</div>
-
-            <ul class="flex space-x-4 font-bold">
-                <li><a href="/" class="text-lime-500">Explore</a></li>
-                <li><a href="/my-books.php" class="hover:underline">My Books</a></li>
-            </ul>
-            <ul>
-                <li><a href="/login.php" class="hover:underline">Login</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <main class="mx-auto max-w-screen-lg space-y-6">
-
-        <form class="w-full flex space-x-2 mt-6">
-            <input type="text" class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-                placeholder="Search...">
-
-            <button type="submit">Search</button>
-        </form>
-
-        <!--Lista de Livros-->
-        <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-
-            <!--Livro-->
-
-            <?php foreach ($livros as $livro) : ?>
-
-                <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                    <div class=" flex">
-                        <div class="w-1/3">imagem</div>
-
-                        <div class="space-y-1">
-                            <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro["titulo"] ?></a>
-                            <div class="text-xs italic"><?= $livro['autor'] ?></div>
-                            <div class="text-xs italic">(3) Avaliações</div>
-                        </div>
-                    </div>
-                    <div class="text-sm mt-2">
-                        <?= $livro['descricao'] ?>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-
-        </section>
-
-    </main>
-</body>
-
-</html>
+require "controllers/{$controller}.controller.php";
